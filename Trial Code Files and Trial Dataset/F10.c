2a112e08bb3c9d1701b2f194004aacfcdf403e15
Nishant Puri <nishant99puri@gmail.com>
2020-04-30 23:39:35 +0530

    Trial Code Files and Trial Dataset
    Trial Code Files and Trial Dataset

---
+++ Trial Code Files and Trial Dataset/F10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a=0;
+    //as.flasjdhfjasdf
+    /*
+    blagagag
+    */
+    /*slakjflasdf;lkj*/
+    char b='w';
+    float c=3.601;
+    printf("%d",&a);
+    printf("%f",&c);
+    printf("%c",&b);
+    return 0;
+}
---
+++ Trial Code Files and Trial Dataset/F5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+//higuysS
+//int j;
+{
+    int a = 0;
+    char b = 'w';
+    float c = 3.601;
+    printf("%d", &a);
+    printf("%f", &c);
+    printf("%c", &b);
+    return 0;
+}
